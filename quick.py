5af9fcd9ada9997c2139fa1aabd519899f8a88fa
ben <benscotstaveley@gmail.com>
2026-06-06 08:56:14 -0700

    dead code removal and some print output reduction

--- quick.py
+++ quick.py
@@ -3,7 +3,6 @@ from llama_cpp import Llama
 import llama_cpp
 import inspect
 import copy
-from jinja2 import Template
 from config import Config
 from llm_manager import LlmManager
 
@@ -11,28 +10,8 @@ print("\n----------Llama------------")
 print(inspect.signature(Llama))
 
 config = Config() ;
-print("-----init object-----\n" + repr(config))
-llm_obj = LlmManager(config)
 
 print("----llama_cpp version: " + llama_cpp.__version__)
-print(dir(llm_obj._llm))
-
-# Note for later: Also note for later — kv_cache_seq_rm and kv_cache_seq_cp are both there, which is exactly what we'll need for Step 2's bookmark/rollback mechanism. Good to know they're accessible.
-
-#for first-time devel
-# print("\n----------Llama------------")
-# print(inspect.signature(Llama))
-# print("\n----------eval------------")
-# print(inspect.signature(llm.eval))
-# print("\n----------dir(llm)------------")
-# print(dir(llm))
-# print("\n----------sample------------")
-# print(inspect.signature(llm.sample))
-# print("\n----------tokenize------------")
-# print(inspect.signature(llm.tokenize))
-# print("\n----------detokenize------------")
-# print(inspect.signature(llm.detokenize))
-# sys.exit()
 
 messages = [
     {"role": "system", "content": config.system_behavior_prompt + config.system_formatting_prompt},
@@ -63,8 +42,4 @@ while True:
     messages.append({"role": "assistant", "content": reply_brief})
 
 # end of while forever
-
-
-
-#print("user input, re-detokenized:-"+llm.detokenize(tokens).decode("utf-8", errors="ignore")+"-")
 
