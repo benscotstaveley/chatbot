4d50f9429eb312a37f99599f773c34e826886c69
ben <benscotstaveley@gmail.com>
2026-06-14 11:56:17 -0700

    enhance correlation runner script: - take command line args to set temperature zero/nonzero - likewise minimal top-k and min-p sampling control - likewise for verbosity - make this more robust by preferring bash arrays to strings

--- correlation/run_llama.sh
+++ correlation/run_llama.sh
@@ -1,65 +1,65 @@
 #!/bin/bash
 
+set -euo pipefail
+
+usage() {
+    echo "Usage: $0 [-v] [-k] [-m] prompt_suite" >&2
+    echo "  -v   pass args to llama-cli appropriate for verbose output" >&2
+    echo "  -k   pass args to llama--cli appropriate for top-k sampling (t>0, top-k>0, etc)" >&2
+    echo "  -m   pass args to llama-cli appripriate for min-p sampling" >&2
+    exit 1
+}
+
 MODEL_DIR=/mnt/models_nvme/models/
-PROMPT_DIR=./prompt_simple
-CTX=24576  # default
+MODEL=$MODEL_DIR/cognitivecomputations_Dolphin-Mistral-24B-Venice-Edition-Q4_K_M.gguf
+N_GPU_LAYERS=24
+CTX=24576
 
-# be sure to only use if all the weights fit in VRAM!  and note
-# it will confuse nvtop's memory reporting.
-#export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1
-# this isn't nearly as good as the gold standard one.  it confuses physical
-# locations of characters, (who is present)
+# be terse and deterministic by default; override with -v and -r
+VERBOSITY_ARGS=()
+DETERMINISTIC_SETTINGS=(--temp 0.0 --top-p 1.0 --top-k  1 --min-p 0.0 --typical_p 1.0)
+TOPK_SETTINGS=(--temp 0.9 --top-p 1.0 --top-k 40 --min-p 0.0 --typical_p 1.0)
+MINP_SETTINGS=(--temp 0.9 --top-p 1.0 --top-k  0 --min-p 0.2 --typical_p 1.0)
+RANDOMNESS_ARGS=(${DETERMINISTIC_SETTINGS[@]})
 
-# https://huggingface.co/bartowski/Qwen2.5-32B-Instruct-GGUF/resolve/main/Qwen2.5-32B-Instruct-Q4_K_M.gguf
-# note to self: get rid of thought blocks for qwen
-#MODEL=$MODEL_DIR/Qwen2.5-32B-Instruct-Q4_K_M.gguf
-#N_GPU_LAYERS=24 ; CTX=24576
+while getopts "vkm" opt; do
+    case "${opt}" in
+        v) VERBOSITY_ARGS=(--verbose-prompt -v) ;;
+        k) RANDOMNESS_ARGS=(${TOPK_SETTINGS[@]}) ;;
+        m) RANDOMNESS_ARGS=(${MINP_SETTINGS[@]}) ;;
+        *) usage ;;
+    esac
+done
+shift "$((OPTIND - 1))"
 
-#MODEL=$MODEL_DIR/TheDrummer_Cydonia-24B-v4.1-IQ4_XS.gguf
-#N_GPU_LAYERS=24 ; CTX=24576
+if [[ $# -ne 1 ]]; then
+    usage
+    exit 1
+else
+    PROMPT_DIR=$1
+fi     
 
-#MODEL=$MODEL_DIR/Midnight-Miqu-70B-v1.5-Q3_K_M.gguf
-#N_GPU_LAYERS=24 ; CTX=16384
-
-MODEL=$MODEL_DIR/L3.2-Rogue-Creative-Instruct-Uncensored-Abliterated-7B-D_AU-IQ4_XS.gguf
-N_GPU_LAYERS=24 ; CTX=24576
-
-#MODEL=$MODEL_DIR/cognitivecomputations_Dolphin-Mistral-24B-Venice-Edition-Q4_K_M.gguf
-#N_GPU_LAYERS=24 ; CTX=24576
-
-#MODEL=$MODEL_DIR/qwen3-30b-a3b-arliai-rpr-v4-fast-q6_k.gguf
-#N_GPU_LAYERS=20 ; CTX=24576
-
-#MODEL=$MODEL_DIR/Qwen3-14B-Base.Q5_K_M.gguf
-#N_GPU_LAYERS=999 ; CTX=8192
-
-# i suspect the chat template is wrong for this one
-#MODEL=$MODEL_DIR/mythomax-l2-13b.Q6_K.gguf
-#N_GPU_LAYERS=26 ; CTX=8192
-
-#MODEL=$MODEL_DIR/dolphin-2.2.1-mistral-7b.Q6_K.gguf
-#N_GPU_LAYERS=999 ; CTX=24576
-
-SYS_BEHAVIOR_PROMPT="$(cat $PROMPT_DIR/sys_behavior.txt)
-SYS_FORMATTING_PROMPT="$(cat $PROMPT_DIR/sys_formatting.txt)
+SYS_BEHAVIOR_PROMPT="$(cat $PROMPT_DIR/sys_behavior.txt)"
+SYS_FORMATTING_PROMPT="$(cat $PROMPT_DIR/sys_formatting.txt)"
 SYS_PROMPT="$SYS_BEHAVIOR_PROMPT$SYS_FORMATTING_PROMPT"
 INITIAL_PROMPT=$(cat $PROMPT_DIR/init.txt)
 
-../../../llama.cpp-latest/build/bin/llama-cli \
-  -m $MODEL \
-  --n-gpu-layers $N_GPU_LAYERS \
-  --ctx-size $CTX \
-  --flash-attn on \
-  --temp 0.0 \
-  --top-p 1.0 \
-  --top-k 1 \
-  --min-p 0.0 \
-  --typical_p 1.0 \
-  --repeat-penalty 1.0 \
-  --conversation \
-  --color on \
-  --system-prompt "$SYS_PROMPT"   \
-  --prompt "$INITIAL_PROMPT" \
-  --seed 1 \
-  "$@"
+CMD=(
+../../../llama.cpp-latest/build/bin/llama-cli
+  -m "$MODEL"
+  --n-gpu-layers "$N_GPU_LAYERS"
+  --ctx-size "$CTX"
+  --flash-attn on
+  ${RANDOMNESS_ARGS[@]}
+  --repeat-penalty 1.0
+  --conversation
+  --color on
+  --system-prompt "$SYS_PROMPT" 
+  --prompt "$INITIAL_PROMPT"
+  --seed 1
+  ${VERBOSITY_ARGS[@]}
+)
+echo "${CMD[@]}"
+"${CMD[@]}"
+
 
