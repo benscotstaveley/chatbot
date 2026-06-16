fb48892311fc8761511e4ccd4fccd795775612bb
ben <benscotstaveley@gmail.com>
2026-06-16 16:53:22 -0700

    add ability to process files specified on the command line to run_tests; add tests_longer suite

--- correlation/run_tests.sh
+++ correlation/run_tests.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if (( $# > 0 )); then
+    TESTS=$@
+else
+    TESTS=`find . -type d -name test_\*`
+fi
 
-for test in `find . -type d -name test_\*`; do
+for test in $TESTS ; do
     echo $test
     cd $test
     python ../../main.py --single-shot --dump-prompts > chatbot.log
