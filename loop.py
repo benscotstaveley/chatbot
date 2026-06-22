66db842657b541f7eafe487ffdcc66667d7d8a13
ben <benscotstaveley@gmail.com>
2026-06-21 20:14:41 -0700

    WIP backup

--- loop.py
+++ loop.py
@@ -2,6 +2,7 @@ from logging import getLogger
 from config import Config
 from llm_manager import LlmManager
 from character import Character, choose_speaker, SpeakerRole, initialize_speaking_order, format_wts, update_speaking_order_for_delay
+import copy
 
 def loop(config:Config, llm_manager:LlmManager, roster:{Character}):
 
@@ -45,11 +46,112 @@ def loop(config:Config, llm_manager:LlmManager, roster:{Character}):
                 # reply_detailed = llm_manager.generate_chat_reply(messages_iter, config)
                 # logger.debug("-------detailed reply:-------\n" + reply_detailed)
 
+                # invariant: upon completion of Human role, 
+
             case SpeakerRole.NPC:
-                reply = llm_manager.generate_chat_reply(messages=messages, config=config, stream=stream)
+
+                # note that DIRECTOR is the name of a label that can appear in 'user' roles and is
+                # explained early in the system prompt as an authoritative director of what the LLM
+                # is to do now.
+
+                # at this point, the last entry in 'messages' could have a user or assistant role.
+                # NPC-after-NPC is the most common case where we get here with an assistant role at
+                # the end of 'messages'; getting here after a HUMAN has spoken in the prior turn is
+                # an example of a user role at the end of 'messages'.  Since this turn is NPC, we
+                # start with adding a '[DIRECTOR]' tag asking the LLM for the very brief indication
+                # of intent as to what this turn's NPC will do and/or say.  Note that [DIRECTOR] is
+                # always classified as role:user.  Therefore in order to maintain strict
+                # user/assistant alternation, if the last entry in 'messages' is role:assistant we
+                # simply append a user entry to the 'messages' list.  But if the last entry is
+                # already role:user, we do not append a new entry, but instead append the [DIRECTOR]
+                # instruction to the end of the content field of the last entry.  Whatever we do, we
+                # record it so we can unwind it.
+
+                prompt_intent = f"[DIRECTOR] Considering the goals and most recent emotional state" \
+                                 f" of {this_speaker.name}, provide a concise desciription of what" \
+                                 f" {this_speaker.name} will say and do right now"
+                bookmark_turn = len(messages)
+                if messages[-1]['role'] == "user":
+                    messages[-1]['content'] += "\n" + prompt_intent
+                else:
+                    messages.append({"role":user, content:prompt_intent})
+
+                # eventually: add an enhancement where we pre-populate the assistant turn with
+                # "[NAME] ".  this will require some jinja magic.  it needs to be done
+                # post-render(), so in llm_manager.generate_chat_reply().
+                postrender_text = ""
+
+                # at this point we are in a part of 'messages' that will be rolled back.
+                # get a statement of intent suitable for verifying.
+
+                # validation loop
+                bookmark_validate = len(messages)
+                validate_attempts=3
+                zero_temp_config = copy.copy(config)  # shallow copy is OK as we change only temp
+                zero_temp_config.temp = 0.0
+                while validate_attempts > 0 :
+
+                    # at this point, either from first iteration or from end of prior iteration,
+                    # we have 'messages' ending with user: [DIRECTOR].
+                    
+                    reply = llm_manager.generate_chat_reply(messages=messages, config=zero_temp_config, stream=False) # TODO(ben) pass postrender_text
+                    best_statement_of_intent = reply
+
+                    # this becomes the model's (ephemeral) statement of intent, initial attempt
+                    messages.append({"role":"assistant", "content": postrender_text + reply})
+
+                    # validate
+                    messages.append({"role":"user",
+                                    "content":f"[DIRECTOR] Consider the above statement of what {this_speaker.name} intends to say and do." \
+                                     f" Is it consistent with all goals, beliefs, and current mindset of {this_speaker.name}?" \
+                                     f" Reply with one word: YES or NO"
+                                    }
+                                   )
+                    reply = llm_manager.generate_chat_reply(messages=messages, config=zero_temp_config, stream=False)
+
+                    match reply:
+                        case "YES":
+                            break
+                        case "NO":
+                            pass
+                        case _:
+                            logger.info(f"garbled response from LLM (should be YES or NO: {reply}")
+                            # fall through to NO (fail) case
+
+                    # lay the groundwork for another attempt
+                    message.append({"role":"assistant", "content": "NO"})
+                    message.append({"role":"user", "content": "[DIRECTOR] State briefly why the intended action is unacceptable."})
+                    reply = llm_manager.generate_chat_reply(messages=messages, config=zero_temp_config, stream=False)
+                    message.append({"role":"assistant", "content": reply})
+                    logger.info(f"LLM rejected its statement of intent because: {reply}")
+                    message.append({"role":"user",
+                                    "content":f"[DIRECTOR] Upon consideration, you found your previous statement of intent for {this_speaker.name}" \
+                                    f" to be unacceptable." \
+                                    f" Considering again the goals and most recent emotional state" \
+                                    f" of {this_speaker.name}, provide a concise desciription of what" \
+                                    f" {this_speaker.name} will say and do right now"
+                                    }
+                                   )
+                    reply = llm_manager.generate_chat_reply(messages=messages, config=zero_temp_config, stream=False)
+
+                    validate_attempts -= 1
+
+                # end of validation loop.  exit with validate_attempts>0: success, ==0: fail.
+                # either way, best_statement_of_intent is as good as we're going to get.
+                # TODO: should we do something in fail case other than log it?
+                logger.warning(f"validation repeatedly failed at turn {bookmark_turn}")
+                del messages[bookmark_validate:]
+                messages.append({"role":"assistant", content: best_statement_of_intent})
+                message.append({"role":"user", "content": f"[DIRECTOR] Given the above statement of intent for {this_speaker.name}," \
+                                f" write a complete response for {this_speaker.name} that embodies this intent and aligns with" \
+                                f" the speaking style of {this_character.name} and stylistic requirements of the current situation."
+                                })
+                
+                reply = llm_manager.generate_chat_reply(messages=messages, config=config, stream=stream)# TODO: preload with name of character
                 if not stream:
                     print(reply)
-                messages.append({"role": "assistant", "content": reply})
+                del messages[bookmark_turn:]
+                messages.append({"role":"assistant", "content":reply})
 
             case SpeakerRole.NARRATOR:
                 if turn_number==0:
